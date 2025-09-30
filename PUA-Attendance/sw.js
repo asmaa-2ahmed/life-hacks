93764dbd48b484706fbf9a843f827bc3304dd290
asmaa-2ahmed <147042364+asmaa-2ahmed@users.noreply.github.com>
2025-09-30 22:35:09 +0300

    Add files via upload

---
+++ PUA-Attendance/sw.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = 'attendance-cache-v1';
+const PRECACHE_URLS = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/script.js',
+  '/manifest.json',
+  '/icon-image.png',
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(PRECACHE_URLS))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+      )
+    )
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => {
+      return resp || fetch(event.request);
+    })
+  );
+});
